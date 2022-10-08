ede75d9ebe27d2b88440ef28be7375afff983312
W. PINHEIRO <wesley.pinheiro_2999@hotmail.com>
2022-10-08 14:12:33 -0300

    Java

---
+++ ExerciciosJava/src/exerciciosjava/ExerciciosJava.java
@@ -0,0 +1,13 @@
+
+package exerciciosjava;
+
+import javax.swing.JOptionPane;
+
+public class ExerciciosJava {
+
+    public static void main(String[] args) {
+       JOptionPane.showMessageDialog(null, "Olá, Mundo!");
+        
+    }
+    
+}
