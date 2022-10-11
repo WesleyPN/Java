86e48db039a9c5eff4d27bc5efd20af49656e53a
W. PINHEIRO <wesley.pinheiro_2999@hotmail.com>
2022-10-11 20:31:03 -0300

    Exercicios

---
+++ IdiomaSistema/src/idiomasistema/IdiomaSistema.java
@@ -0,0 +1,12 @@
+
+package idiomasistema;
+
+import java.util.Locale;
+public class IdiomaSistema {
+
+    
+    public static void main(String[] args) {
+        System.out.println("O idioma do sistema é "+Locale.getDefault().getDisplayLanguage());
+    }
+    
+}
